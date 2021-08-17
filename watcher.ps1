12a8a6708c50e2d311413b994bb57e4a99b326ba
Hewston Fox <hewstonfox@gmail.com>
2021-08-17 22:33:25 +0300

    update(watcher): add initial env setup

--- watcher.ps1
+++ watcher.ps1
@@ -1,21 +1,36 @@
-$activator_exist = Test-Path -Path .\env_activator.ps1
-if ($activator_exist)
-{
-    .\env_activator.ps1
-}
-else
+if (-Not(Test-Path -Path .\env_activator.ps1))
 {
     $venv_path = Read-Host -Prompt 'Enter your python virtual environment folder'
     while (-Not(Test-Path -Path "$venv_path\Scripts\"))
     {
-        echo "Invalid path ($venv_path\Scripts\)"
+        Write-Output "Invalid path ($venv_path\Scripts\)"
         $venv_path = Read-Host -Prompt 'Try again'
     }
 
-    echo "
+    Write-Output "
 Set-Location '$venv_path\Scripts\'
 .\activate
 Set-Location $PSScriptRoot
     " > env_activator.ps1
-    .\env_activator.ps1
 }
+if (-Not(Test-Path -Path .\.env.develop))
+{
+    $env = ""
+    $sep = "="
+    $requiredFields = "BOT_TOKEN", "NOTIFICATION_CHAT"
+    foreach ($line in Get-Content .\.env)
+    {
+        $key, $value = $line.Split($sep)
+        if ($key -eq "MODE")
+        {
+            $env += $key + $sep + "development" + "`n"
+        }
+        elseif ($requiredFields.Contains($key))
+        {
+            $value = Read-Host -Prompt $key
+            $env += $key + $sep + $value + "`n"
+        }
+    }
+    [IO.File]::WriteAllLines(".\.env.develop", $env)
+}
+.\env_activator.ps1
