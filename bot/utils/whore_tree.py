1616f5e167c4530b9a1dbff2bd7aaa69965a4cb2
Youshko Vladimir <youshko.vladimir@gmail.com>
2021-08-18 15:35:52 +0300

    fix: fix whore tree

--- bot/utils/whore_tree.py
+++ bot/utils/whore_tree.py
@@ -8,20 +8,18 @@ class WhoreTree:
 
     def is_fucked(self, prev_fuckers: list['WhoreTree'] = []) -> bool:
         if self in prev_fuckers:
-            return not not len(prev_fuckers) % 2
+            return not len(prev_fuckers) % 2
         return any([not fucker.is_fucked(prev_fuckers + [self]) for fucker in self.fuckers])
 
 
 def create_whore_tree(action, actions: list, checked: list[WhoreTree] = []):
-    fucks = [act for act in actions if act.is_blocker]
-
     if len(fucker := [ch for ch in checked if ch.fucker == action.actor]):
         return fucker[0]
 
     fucker = WhoreTree(action.actor)
     fucker.fuckers = [
-        create_whore_tree(fuck, fucks, checked + [fucker])
-        for fuck in fucks if fuck.target == action.actor
+        create_whore_tree(fuck, actions, checked + [fucker])
+        for fuck in actions if fuck.target == action.actor and fuck.is_blocker
     ]
 
     return fucker
