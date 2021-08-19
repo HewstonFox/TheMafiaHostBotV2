d02afcd32df5c3025e250e285227a124b322af75
Youshko Vladimir <youshko.vladimir@gmail.com>
2021-08-19 17:54:52 +0300

    feat: Don, Mafia role

--- bot/models/Roles/Don.py
+++ bot/models/Roles/Don.py
@@ -1,5 +1,10 @@
+from bot.controllers.ActionController.Actions.VoteAction import DonKillVoteAction
 from bot.models.Roles.Mafia import Mafia
+from bot.types import ChatId
 
 
 class Don(Mafia):
     shortcut = 'don'
+
+    def affect(self, other: ChatId):
+        self.action = DonKillVoteAction(self, self.players[other])
--- bot/models/Roles/Mafia.py
+++ bot/models/Roles/Mafia.py
@@ -1,7 +1,18 @@
+from bot.controllers.ActionController.Actions.VoteAction import MafiaKillVoteAction
+from bot.controllers.MenuController.MenuController import MenuController
 from bot.models.Roles.BaseRole import BaseRole
+from bot.types import ChatId
+from bot.utils.roles import get_players_list_menu
 
 
 class Mafia(BaseRole):
     shortcut = 'maf'
 
-    pass
+    def affect(self, other: ChatId):
+        self.action = MafiaKillVoteAction(self, self.players[other])
+
+    async def send_action(self):
+        await MenuController.show_menu(**get_players_list_menu(
+            self,
+            lambda x: all((x.alive, self.user.id != x.user.id, not isinstance(x, Mafia)))
+        ))
