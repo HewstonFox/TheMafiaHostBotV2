84aa22db3bf868bb080f749b53bbd2a5ceb6bb75
YoushkoVladimir <youshko.vladimir@gmail.com>
2022-05-18 00:57:44 +0300

    fix(bum): typo

--- bot/emoji/__init__.py
+++ bot/emoji/__init__.py
@@ -22,7 +22,7 @@ class ERole(DataclassFromDict):
 
 @dataclass
 class EAction(DataclassFromDict):
-    bom: str = field_from_dict()
+    bum: str = field_from_dict()
     kill: str = field_from_dict()
     don: str = field_from_dict()
     maf: str = field_from_dict()
