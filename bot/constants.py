a5656e8963ebe53b12b0edca1f4b1e23b2ccd46f
YoushkoVladimir <youshko.vladimir@gmail.com>
2022-05-23 01:36:45 +0300

    fix: add bot token to webhook route

--- bot/constants.py
+++ bot/constants.py
@@ -2,8 +2,8 @@ from config import env
 import sys
 
 IS_WEBHOOK = len(sys.argv) > 1
-WEBHOOK_PATH = "/webhook"
+WEBHOOK_PATH = f'/webhook/{env.BOT_TOKEN}'
 WEBHOOK_HOST = sys.argv[2] if len(sys.argv) > 2 else env.BASE_URI
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = sys.argv[1] if IS_WEBHOOK else env.PORT
