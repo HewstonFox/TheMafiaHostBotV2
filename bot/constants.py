3f71905fb5609d5929d90ee3ffb056048b1c8c35
YoushkoVladimir <youshko.vladimir@gmail.com>
2022-05-23 01:24:28 +0300

    fix: webapp host

--- bot/constants.py
+++ bot/constants.py
@@ -5,5 +5,5 @@ IS_WEBHOOK = len(sys.argv) > 1
 WEBHOOK_PATH = "/webhook"
 WEBHOOK_HOST = sys.argv[2] if len(sys.argv) > 2 else env.BASE_URI
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
-WEBAPP_HOST = '127.0.0.1'
+WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = sys.argv[1] if IS_WEBHOOK else env.PORT
