a30c2775122710d9cb605e7612f74346c7a9c4fd
Youshko Vladimir <youshko.vladimir@gmail.com>
2023-02-09 22:40:18 +0200

    chore: update libs

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.11
+
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+
+RUN pip3 install -r requirements.txt
+
+COPY app app
+COPY assets assets
+COPY bot bot
+COPY .env.prod .env
+COPY config.py config.py
+COPY main.py main.py
+
+CMD ["python3", "main.py"]
--- bot/constants.py
+++ bot/constants.py
@@ -1,9 +1,11 @@
 from config import env
 import sys
 
-IS_WEBHOOK = len(sys.argv) > 1
 WEBHOOK_PATH = f'/webhook/{env.BOT_TOKEN}'
-WEBHOOK_HOST = sys.argv[2] if len(sys.argv) > 2 else env.BASE_URI
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = sys.argv[1] if IS_WEBHOOK else env.PORT
+
+ARGS_LEN = len(sys.argv)
+WEBAPP_PORT = sys.argv[1] if ARGS_LEN > 1 else env.PORT
+WEBHOOK_HOST = sys.argv[2] if ARGS_LEN > 2 else env.BASE_URI
+IS_WEBHOOK = WEBAPP_PORT and WEBHOOK_HOST
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}' if IS_WEBHOOK else None
